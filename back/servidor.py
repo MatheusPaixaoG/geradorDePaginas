7ddf86decbfc68d1028b8f150091c86b61a55523
Matheus Paixão <mpgs@cin.ufpe.br>
2023-09-25 22:22:06 -0300

    removendo app.run

--- back/servidor.py
+++ back/servidor.py
@@ -123,6 +123,3 @@ def handle_request():
         )
         response.headers.add('Access-Control-Allow-Origin', '*')
         return response
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
